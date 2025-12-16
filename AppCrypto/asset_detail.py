d20396d4e522bac077ec0b928972af41339eec18
Wildaga Inversiones <gutierrez.madrid.wlmer@gmail.com>
2025-12-16 18:55:03 -0300

    Ajustado para buscar fecha inmediata superior a la fecha pasada como parámetro Si hay múltiples documentos para esa fecha, descarga el más reciente Agregado parámetro display_log para controlar mensajes de consola Limpieza automática de nombres duplicados " (1)", " (2)", etc. Cambios relacionados con la refactorización de select_sesion()

--- AppCrypto/asset_detail.py
+++ AppCrypto/asset_detail.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from binanceX.utils import config_logging
+from binance.utils import config_logging
 
 from prepare_env import get_api_key
 
@@ -10,5 +10,5 @@ config_logging(logging, logging.DEBUG)
 
 api_key, api_secret = get_api_key()
 
-spot_client =Client(api_key, api_secret)
+spot_client = Client(api_key, api_secret)
 logging.info(spot_client.asset_detail())
